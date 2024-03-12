c5f4f274bcc47b117c0a993c7c7e4c836a96d663
michellevit <mvflandin@gmail.com>
2024-03-12 16:15:58 -0700

    Modify urls to serve reactapp

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
@@ -21,5 +23,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='welcome.html'), name='home'),
-]
+    path('', TemplateView.as_view(template_name='index.html'), name='home'), 
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
