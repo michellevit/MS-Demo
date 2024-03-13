3fb234e10e335d62f15877844559f055145f0dfd
michellevit <mvflandin@gmail.com>
2024-03-12 20:15:13 -0700

    Update backend

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,6 @@ from .views import *
 urlpatterns = [
     path('', TemplateView.as_view(template_name='welcome.html'), name='api-welcome'),
     path('mining-data/', MiningDataListView.as_view(), name='mining-data'),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
     
--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -2,11 +2,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from api.views import get_mining_data
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-    path('', TemplateView.as_view(template_name='welcome.html'), name='home'),
- ]
+    path('api/', include('api.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='react-app'), 
+]
