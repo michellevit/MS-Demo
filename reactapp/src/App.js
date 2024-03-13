4030fbcb7209ed1250a39217a44d003f9e802cf9
michellevit <mvflandin@gmail.com>
2024-03-12 20:58:41 -0700

    Remove react routing

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from 'react-router-dom'; 
+// import { Route, Routes } from 'react-router-dom'; 
 import "./App.css";
 import Main from './components/Main';
 
@@ -7,9 +7,10 @@ function App() {
   return (
     <div className="app">
       <div className="container">
-        <Routes>
+        hi
+        {/* <Routes>
           <Route path="/" element={<Main />} />
-        </Routes>
+        </Routes> */}
       </div>
     </div>
   );
